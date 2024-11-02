66050a9feafe399637f80f5f04a58cd769f722aa
James Frech <jamesjfrech@gmail.com>
2024-11-01 21:40:30 -0400

    Added traffic speed and car count

--- predict_speed.py
+++ predict_speed.py
@@ -1,28 +1,36 @@
 import cv2
-from ultralytics import solutions
+from ultralytics.solutions import SpeedEstimator
+from numpy import mean
+from pandas import DataFrame
 
-filename='' # Insert path to video here
-output_filename='output_speeds.avi' # Insert path for an output annotated video with speeds
+# Choose video and create name for output file
+input_video_file = ""
+output_video_file = ""
+output_results = ""
 
-cap = cv2.VideoCapture(filename)
-
+# Load video and get width, height, fps
+cap = cv2.VideoCapture(input_video_file)
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-video_writer = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+# initialize writer for output video
+video_writer = cv2.VideoWriter(output_video_file, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
-# Get pixel region where you want to calculate speed
-# [(low_x,high_y),(high_x,high_y),(high_x,low_y),(low_x,low_y)]
+# region is [(left,top),(right,top),(right,bottom),(left,bottom)]
 speed_region = [(int(w/3),int(4*h/5)),(w,int(4*h/5)),(w,0),(int(w/3),0)]
+speed = SpeedEstimator(model="yolo11n.pt", region=speed_region, show=False)
 
-# Initalize YOLO model for speed detection of cars
-speed = solutions.SpeedEstimator(model="yolo11n.pt", region=speed_region, show=True)
-
+frame_speeds=[]
 while cap.isOpened():
     success, im0 = cap.read()
-    # If frame was read, estimate speeds in frame and write annotated frame to video
+
     if success:
         out = speed.estimate_speed(im0)
+        # Get only values not equal to 0 and convert to mph
+        # 1 km = 0.621371 mi
+        frame_speed = [i*0.621371 for i in speed.spd.values() if i != 0]
+        frame_speeds = frame_speeds + frame_speed
+
         video_writer.write(im0)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -33,3 +41,12 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
+
+# Get count of cars and average traffic speed
+n_cars = len(speed.trkd_ids)
+# Take the average of all speeds computed in video
+traffic_speed = mean(frame_speeds)
+
+# Save results
+results = DataFrame({'n_cars':n_cars,'traffic_speed':traffic_speed},index=[0])
+results.to_csv(output_results,index=False)
