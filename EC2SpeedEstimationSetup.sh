71f4f9c46d16ae1fb6b45b214492b004c2fa7a14
James Frech <jamesjfrech@gmail.com>
2024-11-25 21:50:07 -0500

    Added comments for EC2 setup

--- EC2SpeedEstimationSetup.sh
+++ EC2SpeedEstimationSetup.sh
@@ -16,6 +16,9 @@ sudo yum install cronie cronie-anacron
 sudo yum install mesa-libGL
 
 # Create requirements.txt
+# Use CPU only pytorch for t2.micro instance without any GPUs and not enough
+# space for full pytorch
+# Need numpy < 2.0
 echo "--extra-index-url https://download.pytorch.org/whl/cpu" > requirements.txt
 echo "numpy==1.23.0" >> requirements.txt
 echo "opencv-python" >> requirements.txt
